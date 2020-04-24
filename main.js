71f258208ef36b74db29c9a8bd62b45b513094ef
TheGodFather00 <63043288+TheGodFather00@users.noreply.github.com>
2020-04-24 10:23:17 +0200

    calc

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function saberi(a,b) {
+    let a = document.getElementById(a)
+    let b = document.getElementById(b)
+    let x = a + b;
+    alert(x);
+  }
+   
+document.getElementById("saberi").onclick=saberi();
